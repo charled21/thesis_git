4f28e959a85d3bf15bea5799cf930dc53e0fdfcc
charled21 <58155981+charled21@users.noreply.github.com>
2021-10-14 03:19:58 +0800

    updated main.php 3:19am

--- main.php
+++ main.php
@@ -25,6 +25,8 @@ else {
      <link href="css/main.css" rel="stylesheet">
      <!-- -------------- Bootstrap ----------------------->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
@@ -148,7 +150,7 @@ else {
                             <div class="row">
                                 <!-- <a class="navbar-main"href="#">HRIS</a>
                                 <a id="nav-main"href="#">-SUBSYS</a> -->
-                                <img class="logo-topbar" src="img/appicon3.png">
+                                <img class="logo-topbar" src="img/logo-1.png">
                             </div>
                                 <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#collapse-items" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
@@ -220,11 +222,13 @@ else {
                         <div class="row landing-container align-items-center" >
                             <!-- <div class="row"> -->
                                 <div class="col-lg-6 col-md-6 text-center">
-                                    <p class="text-style">HUMAN RESOURCE INFORMATION SYSTEM</p>
-
+                                    <p class="mt-4 mb-3" style="font-family: 'Nunito'; font-size: 50px;";>WE ARE NOW HIRING!</p>
+                                    <p class="mt-3 mb-5" style="font-family: 'Nunito'; font-size: 14px;";>“Motorsiklo Sigurado, Alaga Ka Dito” sums up its number one priority — Total Customer Satisfaction is what we always guarantee! </p>
+                                    <!-- <img class="logo-topbar-2 img-fluid" src="img/banner-1.png"> -->
+         
                                     <?php 
                                     if(!isset($_SESSION["username"])){
-                                        echo "<button class=\"btn btn-warning button-style\" data-toggle=\"modal\" data-target=\"#accountmodal\" id=\"gs-btn\">GET STARTED</button>";
+                                        echo "<button class=\"btn btn-warning button-style \" data-toggle=\"modal\" data-target=\"#accountmodal\" id=\"gs-btn\" style=\"font-family: 'Nunito'; font-size: 20px;\";>GET STARTED</button>";
                                     }
                                     else {
                                         //if logged in, welcomes user
@@ -233,9 +237,51 @@ else {
                                     ?>
                                     
                                 </div>
-                                <div class="col-lg-6 col-md-6">
-                                    <img class="img-fluid" src="img/landing_pg_1.png">
+
+                                <!-- # of statistics start -->         
+                                <div class="form-row col-lg-6">
+                                    
+                                <div class="col-xl-5 col-md-12 mb-4 mr-4">
+                                    <div class="card border-left-success shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                    Available Slots</div>
+                                                    <div class="h5 mb-0 font-weight-bold text-gray-800">13</div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <!-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> -->
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
                                 </div>
+
+                                <div class="col-xl-5 col-md-12 mb-4">
+                                    <div class="card border-left-success shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                                    Total Number of Applicants</div>
+                                                    <div class="h5 mb-0 font-weight-bold text-gray-800">121</div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <!-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> -->
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+
+                                 
+                                
+                                
+                                <!-- # of statistics end -->
+                                
                             <!-- </div> -->
                         </div>
                     </div>
@@ -243,6 +289,11 @@ else {
             </section>
             <!-- ---------------------landing cover end-------------------------- -->
 
+            <!-- data analytics start  -->
+
+
+            <!-- data analytics end -->
+ 
             
 
             <!-----------------------scripts start-------------------------- -->
@@ -250,6 +301,9 @@ else {
             <script src="js/jquery-3.4.1.min.js"></script>
             <script src="js/bootstrap.min.js"></script>
             <script src="js/jquery.easing.min.js"></script>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
             <!-----------------------scripts end-------------------------- -->
 
         </body>
