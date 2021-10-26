e9549854ac912465db22e47c969b6b5301592aa9
charled21 <58155981+charled21@users.noreply.github.com>
2021-10-26 18:50:07 +0800

    updated 10-26-21 6:49pm
    -updated ans-process.js and added several functions to asses display on progressbars and their respective data fetched from sources such as db/other webpages
    -updated answer-process.php to cater the tweaks made to own javascript

--- php/answer-process.php
+++ php/answer-process.php
@@ -5,6 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Application 1 Process</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="/thesis_git/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+     <link
+         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+         rel="stylesheet">
+ 
+     <link href="/thesis_git/css/main.css" rel="stylesheet">
+ 
+     <!-- Custom styles for this template-->
+     <link href="/thesis_git/css/sb-admin-2.min.css" rel="stylesheet">
+     <link href="/thesis_git/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -18,6 +31,39 @@ $opt = array();
             result[$x] = ". $opt[$x]. " </script>";
         }
 ?>
+
+<label for="mindBar">Mind Bar</label>
+<div id="mindBar" class="progress"></div>
+<label for="enerBar">Energy Bar</label>
+<div id="enerBar" class="progress"></div>
+<label for="natBar">Nature Bar</label>
+<div id="natBar" class="progress"></div>
+<label for="tactBar">Tactics Bar</label>
+<div id="tactBar" class="progress"></div>
+<label for="identBar">Identity Bar</label>
+<div id="identBar" class="progress"></div>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="/thesis_git/vendor/jquery/jquery.min.js"></script>
+    <script src="/thesis_git/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="/thesis_git/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="/thesis_git/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="/thesis_git/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="/thesis_git/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="/thesis_git/js/demo/datatables-demo.js"></script>
 
 <script type="text/javascript" src="/thesis_git/js/ans-process.js"></script>
 
