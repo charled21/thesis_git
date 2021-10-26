7becd5d0823f5fdd005787d60d16c6fb267e80c9
charled21 <58155981+charled21@users.noreply.github.com>
2021-10-26 18:10:35 +0800

    update 10-26-21 6:09pm
    -added own javascript to compute scores passed from radio buttons in examination page ans-process.js
    -edited answer-process.php to refer to ans-process js and edited it to pass fetch correct value from radio buttons

--- php/answer-process.php
+++ php/answer-process.php
@@ -14,39 +14,12 @@ $opt = array();
         echo "<script>const result = []; </script>";
         for($x = 1; $x <=60; $x++){
             $opt[$x]=$_POST["opt_" . $x . ""];
-            
             echo "<script> 
-            
             result[$x] = ". $opt[$x]. " </script>";
         }
-
-        // $opt_1 = $_POST['opt_1'];
-        // $opt_2 = $_POST['opt_2'];
-        // $opt_3 = $_POST['opt_3'];
-        // $opt_4 = $_POST['opt_4'];
-        // $opt_5 = $_POST['opt_5'];
-        // echo "
-        // <script>
-        // opt_1=$opt1;
-        // opt_2=$opt2;
-        // opt_3=$opt3;
-        // opt_4=$opt4;
-        // opt_5=$opt5;
-        // </script>";
-
-        //print_r("Q1: $opt1 <br>Q2: $opt2 <br>Q3: $opt3 <br>Q4: $opt4 <br>Q5: $opt5");
-    
 ?>
 
-<script>
+<script type="text/javascript" src="/thesis_git/js/ans-process.js"></script>
 
-
-for(let i = 1; i <= 60; i++){
-    console.log(result[i]);
-}
-    
-
-
-</script>
 </body>
 </html>
