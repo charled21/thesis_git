ebcf3d20aa556b163b8f89d6157aad5556033ab2
charled21 <58155981+charled21@users.noreply.github.com>
2021-11-01 21:47:38 +0800

    update as of 11-1-21 9:47pm
    -added export to CSV function to csv-export.php

--- php/csv-export.php
+++ php/csv-export.php
@@ -20,8 +20,11 @@
     <button type="submit" id="view_btn">View</button>
     </form>
 
-    <div id="content"></div>
-    <div id="app_id"></div>
+    <div id="content">
+    </div>
+    <button id="exp_csv">Export to CSV</button>
+    
+    
     
     
     <script src="/thesis_git/vendor/jquery/jquery.min.js"></script>
@@ -42,7 +45,7 @@
             type: "POST",
             data:{"myData":myVar}
             }).done(function(data) {
-                console.log(data);
+                //console.log(data);
                 $('#content').html(data);
             });
 
@@ -55,7 +58,7 @@
             type: "POST",
             data:{"myData":myVar}
             }).done(function(data) {
-                console.log(data);
+                //console.log(data);
                 $('#content').html(data);
             });
 
@@ -68,7 +71,7 @@
             type: "POST",
             data:{"myData":attend_data}
             }).done(function(data) {
-                console.log(data);
+                //console.log(data);
                 $('#content').html(data);
             });
 
@@ -79,5 +82,54 @@
 	});
 </script>
 
+<script>
+    document.getElementById("exp_csv").addEventListener("click", function () {
+	var html = document.querySelector("table").outerHTML;
+	htmlToCSV(html, "records.csv");
+});
+</script>
+
+<script>
+function htmlToCSV(html, filename) {
+        var data = [];
+        var rows = document.querySelectorAll("table tr");
+                
+        for (var i = 0; i < rows.length; i++) {
+            var row = [], cols = rows[i].querySelectorAll("td, th");
+                    
+            for (var j = 0; j < cols.length; j++) {
+                    row.push(cols[j].innerText);
+            }
+                    
+            data.push(row.join(",")); 		
+        }
+
+        downloadCSVFile(data.join("\n"), filename);
+    }
+
+</script>
+
+<script>
+
+    function downloadCSVFile(csv, filename) {
+	var csv_file, download_link;
+
+	csv_file = new Blob([csv], {type: "text/csv"});
+
+	download_link = document.createElement("a");
+
+	download_link.download = filename;
+
+	download_link.href = window.URL.createObjectURL(csv_file);
+
+	download_link.style.display = "none";
+
+	document.body.appendChild(download_link);
+
+	download_link.click();
+}
+
+
+</script>
 </body>
 </html>
